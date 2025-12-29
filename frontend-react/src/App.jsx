88c8708ecc175f5c8aea0f043f9fcb443f2bb835
Arslan Ahmad <arslanahmad2263@gmail.com>
2025-12-29 21:29:01 +0500

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './assets/css/style.css';
+import Header from './components/header.jsx';
+import Footer from './components/footer.jsx';
+import Main from './components/main.jsx';
+
+
+
 
 function App() {
   
 
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
       
-     
     </>
   )
 }
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <>
+        <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy; 2025 Build with ❤️ by Arslan Ahmad</p>
+        </footer>
+     
+    </>
+  )
+}
+
+export default footer
